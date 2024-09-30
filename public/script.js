52c793186f1d314e288767ba4eca2630c01a3b73
DC_Jay <Harrisj22412@hocking.edu>
2024-09-30 11:00:59 +0000

    Add socket.io functionality for user connection in server.js and room.ejs

---
+++ public/script.js
@@ -0,0 +1,9 @@
+const e = require("express");
+
+const socket = io('/');
+
+socket.emit('join-room', ROOM_ID, 10);
+
+socket.on('user-connected', userId => {
+    console.log('User connected: ' + userId)
+})
