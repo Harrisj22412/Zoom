96ede283de5031b3c336a79815f299b3d6e5edb9
DC_Jay <Harrisj22412@hocking.edu>
2024-09-30 11:09:11 +0000

    Add socket.io functionality for user connection in script.js and room.ejs

--- public/script.js
+++ public/script.js
@@ -1,8 +1,15 @@
 const e = require("express");
+const mypeer = new myPeer(undefined,  {
+    host: '/',
+    port: '3001'
+}); 
 
 const socket = io('/');
 
-socket.emit('join-room', ROOM_ID, 10);
+myPeer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id);
+})
+
 
 socket.on('user-connected', userId => {
     console.log('User connected: ' + userId)
